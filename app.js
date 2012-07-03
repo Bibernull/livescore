138b0f0760fd047867d6aaaae126cbf6e0e4c0f3
Biber <biber.np@gmail.com>
2012-07-03 18:14:04 +0200

    phantomjs

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
+var phantom = require('phantom'),
+     fs = require('fs');
+phantom.create(function(ph) {
+  return ph.createPage(function(page) {
+    return page.open("http://www.rezultati.com/iframe/sport.php?sport=soccer&category=0&serial=403", function(status) {
+      console.log("opened site? ", status);         
+ 
+            page.injectJs('http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js', function() {
+                //jQuery Loaded.
+                //Wait for a bit for AJAX content to load on the page. Here, we are waiting 5 seconds.
+                setTimeout(function() {
+                    return page.evaluate(function() {
+                        console.log("hej");
+                        //Get what you want from the page using jQuery. A good way is to populate an object with all the jQuery commands that you need and then return the object.
+                        var leaguesArr = [],
+                        matchesArr = [],
+                        i=0;
+                        
+                        $('.soccer').each(function() {
+                            leaguesArr.push($(this).find('.country .name').html());
+                            $(this).find('tbody tr').each(function() {
 
-/**
- * Module dependencies.
- */
 
-var express = require('express')
-  , routes = require('./routes');
+                              matchesArr[i] = [];
+                              matchesArr[i].push({
+                                      team1: $(this).find('.team-home').text(), 
+                                      team2: $(this).find('.team-away').text(),
+                                      score: $(this).find('.score').text()
+                                    });
+                            });
+                            i++;
+                        });
+                        
 
-var app = module.exports = express.createServer();
+ 
+                        return {
+                            leagues: leaguesArr,
+                            matches: matchesArr
+                        };
 
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+                        
+                    }, function(result) {
+                      fs.writeFile('rezultati.txt', JSON.stringify(result), function(err) {
+                        if(err) throw err;
+                        
+                      ph.exit();
+                      });
+                    });
+                }, 5000);
+ 
+            });
+    });
+    });
 });
-
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
